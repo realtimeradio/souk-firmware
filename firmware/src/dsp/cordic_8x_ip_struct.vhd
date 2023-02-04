94527b2a0bc1f3c08a5540fa706602d172a02c35
Jack <jack@realtimeradio.co.uk>
2023-02-04 12:32:17 +0000

    Add phase 1 TX pipeline
    CORDIC-/LUT-based CW generation

---
+++ firmware/src/dsp/cordic_8x_ip_struct.vhd
@@ -0,0 +1,33 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+library xil_defaultlib;
+use xil_defaultlib.conv_pkg.all;
+entity cordic_8x_ip_struct is
+  port (
+    phase_inc : in std_logic_vector( 64-1 downto 0 );
+    load  : in std_logic_vector( 1-1 downto 0 );
+    clk_1 : in std_logic;
+    ce_1  : in std_logic;
+    dout  : out std_logic_vector( 288-1 downto 0 )
+  );
+end cordic_8x_ip_struct;
+
+architecture structural of cordic_8x_ip_struct is
+  component cordic_8x
+    port (
+      phase_inc  : in std_logic_vector( 64-1 downto 0 );
+      load  : in std_logic_vector( 1-1 downto 0 );
+      clk   : in std_logic;
+      dout  : out std_logic_vector( 288-1 downto 0 )
+    );
+  end component;
+
+begin
+  cordic_8x_ip_inst : cordic_8x
+  port map (
+    phase_inc => phase_inc, 
+    load      => load, 
+    clk       => clk_1, 
+    dout      => dout
+  );
+end structural; 
