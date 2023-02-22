624a515da72406c85e19367312fde3b72564d941
Jack <jack@realtimeradio.co.uk>
2023-02-22 09:40:10 +0000

    Improve PFB sweep script

--- software/control_sw/test_scripts/ospfb_sweep.py
+++ software/control_sw/test_scripts/ospfb_sweep.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import souk_mkid_readout
@@ -33,7 +34,9 @@ def scan_bin(r, n, p=50, b=4):
         print(f"Sweeping tone {fn+1} of {p}", end=' ')
         set_output_freq(r, freq)
         x = np.fft.fftshift(r.autocorr.get_new_spectra(0, True)[0])
-        print(f"(Maximum power found in bin {x.argmax()})")
+        binstart = 2048 + n - b//2
+        binstop  = 2048 + n - b//2 + b
+        print(f"(Maximum power found in bin {x.argmax()}, getting data from bins {binstart}-{binstop})")
         for i in range(b):
             d[i,fn] = x[2048 + n - b//2 + i]
     return d
@@ -47,17 +50,33 @@ def plot_scan(r, n, p, b, normalize=True):
     plt.legend()
     plt.show()
 
-r = souk_mkid_readout.SoukMkidReadout(HOST, configfile=CONFIGFILE)
-r.program()
-r.initialize()
-r.output.use_cordic()
-r.input.enable_loopback()
-r.pfb.set_fftshift(0b111111110000)
-r.autocorr.set_acc_len(1000)
+def main(host, configfile):
+    r = souk_mkid_readout.SoukMkidReadout(host, configfile=configfile)
+    r.program()
+    r.initialize()
+    r.output.use_cordic()
+    r.input.enable_loopback()
+    r.pfb.set_fftshift(0xffffffff)
+    r.autocorr.set_acc_len(1000)
+    overflow_before = r.pfb.get_overflow_count()
+    plot_scan(r, 100, 200, 6)
+    overflow_after = r.pfb.get_overflow_count()
+    overflow_count = overflow_after - overflow_before
+    print(f"Total FFT overflows during scan: {overflow_count}")
 
-overflow_before = r.pfb.get_overflow_count()
-plot_scan(r, 100, 200, 6)
-overflow_after = r.pfb.get_overflow_count()
-overflow_count = overflow_after - overflow_before
-print(f"Total FFT overflows during scan: {overflow_count}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "A script to sweep a CW tone over a PFB bin and print response",
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+    )
 
+    parser.add_argument("host", type=str, default=HOST,
+        help = "Hostname / IP address of FPGA board to test",
+    )
+
+    parser.add_argument("configfile", type=str, default=CONFIGFILE,
+        help = "Configuration YAML file with which to test",
+    )
+
+    args = parser.parse_args()
+    main(args.host, args.configfile)
