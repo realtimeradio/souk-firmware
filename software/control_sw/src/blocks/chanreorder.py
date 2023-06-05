664896b4c61f72aeead559478b0edddfbb4187aa
JackH <jack@rtr>
2023-06-05 19:47:43 +0000

    v5.5 Test-Benched channel reorderer
    v5.5 also incorporates FFT optimizations to attempt to reduce power consumption

--- software/control_sw/src/blocks/chanreorder.py
+++ software/control_sw/src/blocks/chanreorder.py
@@ -28,6 +28,10 @@ class ChanReorder(Block):
         "zero out this channel"
     :type support_zeroing: bool
 
+    :param parallel_first: If True, the firmware reorders in the parallel signal
+        dimension before the serial dimension.
+    :type parallel_first: bool
+
     """
     _map_format = 'i4' # CASPER library-defined map word format
     _map_reg = 'map1' # CASPER library-defined map name in reorder block
@@ -36,6 +40,7 @@ class ChanReorder(Block):
             n_chans_out=2048,
             n_parallel_chans_in=4,
             support_zeroing=False,
+            parallel_first=False,
             logger=None):
         super(ChanReorder, self).__init__(host, name, logger)
         assert n_chans_in % n_parallel_chans_in == 0
@@ -48,13 +53,36 @@ class ChanReorder(Block):
         self.support_zeroing = support_zeroing
         self.n_chans_in = n_chans_in
         self.n_chans_out = n_chans_out
+        self._parallel_first = parallel_first
         self._n_parallel_chans_in = n_parallel_chans_in
         self._n_serial_chans_in = n_chans_in // n_parallel_chans_in
         # These asserts probably don't catch all configuration issues
         assert n_chans_in % n_chans_out == 0
         self._inout_ratio = n_chans_in // n_chans_out
         assert n_parallel_chans_in % self._inout_ratio == 0
-        self._n_parallel_chans_out = n_parallel_chans_in // self._inout_ratio
+        # If reordering parallel first, the reorder is completed with the
+        # same number of input and output samples in parallel, and then
+        # the decimation occurs after reordering.
+        # If serial reordering first, we decimate the samples prior
+        # to reordering in the parallel dimension.
+        if self._parallel_first:
+            self._n_parallel_chans_out = n_parallel_chans_in
+        else:
+            self._n_parallel_chans_out = n_parallel_chans_in // self._inout_ratio
+
+    def _validate_outmap(self, outmap):
+        outmap = np.array(outmap)
+        # We must load the reorder map in one go, so it should be for all chans
+        if outmap.shape[0] != self.n_chans_out:
+            raise ValueError(f'Input outmap should be {self.n_chans_out} elements long')
+        # Check the output chans are all in the input
+        for outchan in outmap:
+            if outchan >= self.n_chans_in:
+                # Allow -1 as a special case, meaning "set this output to 0"
+                if self.support_zeroing and outchan == -1:
+                    continue
+                raise ValueError(f'Selected channel {outchan} not in input range')
+        return outmap
 
     def set_channel_outmap(self, outmap):
         """
@@ -70,17 +98,7 @@ class ChanReorder(Block):
         :type outmap: list of int
 
         """
-        outmap = np.array(outmap)
-        # We must load the reorder map in one go, so it should be for all chans
-        if outmap.shape[0] != self.n_chans_out:
-            raise ValueError(f'Input outmap should be {self.n_chans_out} elements long')
-        # Check the output chans are all in the input
-        for outchan in outmap:
-            if outchan >= self.n_chans_in:
-                # Allow -1 as a special case, meaning "set this output to 0"
-                if self.support_zeroing and outchan == -1:
-                    continue
-                raise ValueError(f'Selected channel {outchan} not in input range')
+        outmap = self._validate_outmap(outmap)
 
         serial_maps = np.zeros([self._n_parallel_chans_in, self._n_serial_chans_in],
                           dtype='>%s' % self._map_format)
@@ -107,6 +125,218 @@ class ChanReorder(Block):
             out_pstream = outn % self._n_parallel_chans_out
             # Which output serial position would we like outchan to be in
             out_spos = outn // self._n_parallel_chans_out
+            # build maps appropriately
+            debug_msg = f'{outn}->{outchan} Setting input (p:s) {in_pstream}:{in_spos} to {out_pstream}:{out_spos}'
+            self.logger.debug(debug_msg)
+            if self._parallel_first:
+                # Reorder the parallel streams first,
+                # so serial reorder maps should be chosen
+                # based on the "new" parallel stream number
+                s_reorder = out_pstream
+                # Parallel map location reflects original serial position
+                p_map_loc = in_spos
+            else:
+                # Reorder serial first,
+                # so serial map choice reflects original parallel position
+                s_reorder = in_pstream
+                # Parallel ordering reflects new serial position
+                p_map_loc = out_spos
+            if outchan != -1:
+                if serial_maps_written[s_reorder][out_spos] != False:
+                    last_msg = serial_maps_written[s_reorder][out_spos]
+                    self.logger.error('Serial reorder clash!')
+                    self.logger.info(f'Attempted: {debug_msg}')
+                    self.logger.info(f'Previously: {last_msg}')
+                if parallel_maps_written[p_map_loc][out_pstream] != False:
+                    last_msg = parallel_maps_written[p_map_loc][out_pstream]
+                    self.logger.error('Parallel reorder clash!')
+                    self.logger.info(f'Attempted: {debug_msg}')
+                    self.logger.info(f'Previously: {last_msg}')
+                serial_maps[s_reorder, out_spos] = in_spos
+                parallel_maps[p_map_loc, out_pstream] = in_pstream
+                serial_maps_written[s_reorder][out_spos] = debug_msg
+                parallel_maps_written[p_map_loc][out_pstream] = debug_msg
+            else:
+                if self._parallel_first:
+                    # Use special mux input which is tied to 0
+                    if serial_maps_written[s_reorder][out_spos] != False:
+                        last_msg = serial_maps_written[s_reorder][out_spos]
+                        self.logger.error('Serial reorder clash!')
+                        self.logger.info(f'Attempted: {debug_msg}')
+                        self.logger.info(f'Previously: {last_msg}')
+                    # This doesn't actually do anything in firmware, but makes it
+                    # clear in software that we don't care about this channel
+                    serial_maps[s_reorder][out_spos] = -1
+                    serial_maps_written[s_reorder][out_spos] = debug_msg
+                else:
+                    # Use special mux input which is tied to 0
+                    if parallel_maps_written[out_spos][out_pstream] != False:
+                        last_msg = parallel_maps_written[out_spos][out_pstream]
+                        self.logger.error('Parallel reorder clash!')
+                        self.logger.info(f'Attempted: {debug_msg}')
+                        self.logger.info(f'Previously: {last_msg}')
+                    parallel_maps[p_map_loc, out_pstream] = self._n_parallel_chans_in + 1
+                    parallel_maps_written[p_map_loc][out_pstream] = debug_msg
+        for i in range(self._n_parallel_chans_in):
+            self.write(f'reorder_{i}_{self._map_reg}', serial_maps[i].tobytes())
+        self.write('pmap', parallel_maps.flatten().tobytes())
+            
+
+    def get_channel_outmap(self):
+        """
+        Read the currently loaded reorder map.
+
+        :return: The reorder map currently loaded. Entry `i` in this map is the
+            channel number which emerges in the `i`th output position.
+        :rtype: list
+        """
+
+        serial_maps = np.zeros([self._n_parallel_chans_in, self._n_serial_chans_in],
+                          dtype='>%s' % self._map_format)
+        parallel_maps = np.zeros([self._n_serial_chans_in, self._n_parallel_chans_out],
+                          dtype='>B')
+
+        nbytes_s = len(serial_maps[0].tobytes())
+        for i in range(self._n_parallel_chans_in):
+            raw = self.read(f'reorder_{i}_{self._map_reg}', nbytes_s)
+            serial_maps[i] = np.frombuffer(raw, dtype=serial_maps.dtype)
+        nbytes_p = len(parallel_maps.tobytes())
+        raw = self.read('pmap', nbytes_p)
+        parallel_map1d = np.frombuffer(raw, dtype=parallel_maps.dtype)
+        parallel_maps[:,:] = parallel_map1d.reshape(parallel_maps.shape)
+
+        outmap = np.zeros(self.n_chans_out, dtype=int)
+        for outn in range(self.n_chans_out):
+            # Which output parallel stream is output outn in
+            out_pstream = outn % self._n_parallel_chans_out
+            # Which output serial position is output outn in
+            out_spos = outn // self._n_parallel_chans_out
+            if self._parallel_first:
+                in_spos = serial_maps[out_pstream, out_spos] # serial input position
+                if in_spos == -1: # indicates not enabled
+                    in_chan = -1
+                else:
+                    in_pstream = parallel_maps[in_spos, out_pstream]
+                    in_chan = in_spos * self._n_parallel_chans_in + in_pstream
+            else:
+                in_pstream = parallel_maps[out_spos, out_pstream]
+                # Catch special case where input is disabled
+                if in_pstream == self._n_parallel_chans_in + 1:
+                    in_chan = -1 # -1 indicates disabled
+                else:
+                    in_stream = serial_maps[in_pstream, out_spos]
+                    in_chan = in_stream * self._n_parallel_chans_in + in_pstream
+            outmap[outn] = in_chan
+        return outmap
+
+    def set_single_channel(self, outidx, inidx):
+        """
+        Set output channel number ``outidx`` to input number ``inidx``.
+        Do this by reading the total channel map, modifying a single entry,
+        and writing back.
+
+        Example usage:
+            # Set the first channel out of the reorder to 33
+            ```set_single_channel(0, 33)``
+
+        :param outidx: Index of output channel to set.
+        :type outidx: int
+
+        :param inidx: Input channel index to select.
+        :type inidx: int
+        """
+        self.logger.info(f'Setting output {outidx} to channel {inidx}')
+        chanmap = self.get_channel_outmap()
+        chanmap[outidx] = inidx
+        self.set_channel_outmap(chanmap)
+        
+
+    def initialize(self, read_only=False):
+        """
+        Initialize the block.
+
+        :param read_only: If True, this method is a no-op. If False,
+            initialize the block with the identity map. I.e., map channel
+            `n` to channel `n`.
+        :type read_only: bool
+        """
+        if read_only:
+            pass
+        else:
+            if self.support_zeroing:
+                chan_order = np.ones(self.n_chans_out) * -1 # Disable everything
+            else:
+                chan_order = np.arange(0, self.n_chans_in, 2) # output every other channel
+            self.set_channel_outmap(chan_order)
+
+class ChanReorderPS(ChanReorder):
+    """
+    Instantiate a control interface for a Channel Reorder "Parallel First" block.
+
+    :param host: CasperFpga interface for host.
+    :type host: casperfpga.CasperFpga
+
+    :param name: Name of block in Simulink hierarchy.
+    :type name: str
+
+    :param logger: Logger instance to which log messages should be emitted.
+    :type logger: logging.Logger
+
+    :param n_chans_in: Number of channels input to the reorder
+    :type n_chans_in: int
+
+    :param n_chans_out: Number of channels output to the reorder
+    :type n_chans_out: int
+
+    :param n_parallel_chans_in: Number of channels handled in parallel at the input
+    :type n_parallel_chans_in: int
+
+    :param support_zeroing: If True, allow the use of channel index ``-1`` to mean
+        "zero out this channel"
+    :type support_zeroing: bool
+
+    """
+    def set_channel_outmap(self, outmap):
+        """
+        Remap the channels such that the channel outmap[i]
+        emerges out of the reorder map in position i.
+
+        The provided map must be `self.n_chans_out` elements long, else
+        `ValueError` is raised
+
+        :param outmap: The outmap to which data should be mapped. I.e., if
+            `outmap[0] = 16`, then the first channel out of the reordr block
+            will be channel 16. 
+        :type outmap: list of int
+
+        """
+        outmap = self._validate_outmap(outmap)
+
+        serial_maps = np.zeros([self._n_parallel_chans_in, self._n_serial_chans_in],
+                          dtype='>%s' % self._map_format)
+        parallel_maps = np.zeros([self._n_serial_chans_in, self._n_parallel_chans_in],
+                          dtype='>i1')
+        # Keep track of which entries have been written
+        # so we can warn if something is overwritten
+        serial_maps_written = [[False for _ in range(self._n_serial_chans_in)]
+                for _ in range(self._n_parallel_chans_in)]
+        parallel_maps_written = [[False for _ in range(self._n_parallel_chans_in)]
+                for _ in range(self._n_serial_chans_in)]
+        # outn is where we want this channel in the output
+        # outchan is where this channel is at the input
+        for outn, outchan in enumerate(outmap):
+            if outchan != -1:
+                # Which of the parallel input streams contains this channel
+                in_pstream = outchan % self._n_parallel_chans_in
+                # Which input serial position contains this channel
+                in_spos = outchan // self._n_parallel_chans_in
+            else:
+                in_pstream = None
+                in_spos = None
+            # Which output parallel stream would we like outchan to be in
+            out_pstream = outn % self._n_parallel_chans_in
+            # Which output serial position would we like outchan to be in
+            out_spos = outn // self._n_parallel_chans_in
             # build maps appropriately
             debug_msg = f'{outn}->{outchan} Setting input {in_pstream}:{in_spos} to {out_pstream}:{out_spos}'
             self.logger.debug(debug_msg)
@@ -180,43 +410,3 @@ class ChanReorder(Block):
                 in_chan = in_stream * self._n_parallel_chans_in + in_pstream
             outmap[outn] = in_chan
         return outmap
-
-    def set_single_channel(self, outidx, inidx):
-        """
-        Set output channel number ``outidx`` to input number ``inidx``.
-        Do this by reading the total channel map, modifying a single entry,
-        and writing back.
-
-        Example usage:
-            # Set the first channel out of the reorder to 33
-            ```set_single_channel(0, 33)``
-
-        :param outidx: Index of output channel to set.
-        :type outidx: int
-
-        :param inidx: Input channel index to select.
-        :type inidx: int
-        """
-        self.logger.info(f'Setting output {outidx} to channel {inidx}')
-        chanmap = self.get_channel_outmap()
-        chanmap[outidx] = inidx
-        self.set_channel_outmap(chanmap)
-        
-
-    def initialize(self, read_only=False):
-        """
-        Initialize the block.
-
-        :param read_only: If True, this method is a no-op. If False,
-            initialize the block with the identity map. I.e., map channel
-            `n` to channel `n`.
-        :type read_only: bool
-        """
-        if read_only:
-            pass
-        else:
-            if self.support_zeroing:
-                chan_order = np.ones(self.n_chans_out) * -1 # Disable everything
-            else:
-                chan_order = np.arange(0, self.n_chans_in, 2) # output every other channel
-            self.set_channel_outmap(chan_order)
---
+++ software/control_sw/test_scripts/souk_tb_chanreorder.py
@@ -0,0 +1,62 @@
+import numpy as np
+import souk_mkid_readout
+
+HOST = 'rfsoc4x2'
+FPGFILE = '/home/jackh/src/souk-firmware/firmware/src/souk_single_pipeline_4x2/outputs/souk_single_pipeline_4x2_2023-06-02_1627.fpg'
+NCHANS = 10
+ACCLEN = 1024
+VERBOSE = True
+
+r = souk_mkid_readout.SoukMkidReadout(HOST, fpgfile=FPGFILE)
+
+r.initialize()
+print('Enabling TVG')
+r.pfbtvg.tvg_enable()
+print('Writing TVG ramp')
+r.pfbtvg.write_freq_ramp()
+print('Configuring accumulator')
+acc = r.accumulators[0]
+acc.set_acc_len(ACCLEN)
+print('Arming and waiting for sync')
+r.sync.arm_sync()
+r.sync.wait_for_sync()
+
+print('Setting channel map')
+chanmap = np.ones(r.chanselect.n_chans_out, dtype=int)*-1
+sel = np.random.randint(0, r.chanselect.n_chans_in, NCHANS)
+# BEWARE: Channel map liable to create conflicts without sorting
+chanmap[0:NCHANS] = sorted(sel)
+expected_output = np.round(chanmap**2 / 2.**16) # Firmware drops 16 bits
+r.chanselect.set_channel_outmap(chanmap)
+r.mixer.enable_power_mode()
+print('Getting spectra')
+acc.get_new_spectra() # Flush a spectra
+x = acc.get_new_spectra()
+assert x.imag.sum() == 0
+x = x.real / ACCLEN
+
+print('Checking channel order in accumulation')
+passed = True
+for i in range(acc.n_chans):
+    if VERBOSE:
+        print(i, chanmap[i], expected_output[i], x[i])
+    if chanmap[i] != -1:
+        if expected_output[i] != x[i]:
+            passed = False
+
+print('Checking channel order in readback')
+chanmap_readback = r.chanselect.get_channel_outmap()
+for i in range(r.chanselect.n_chans_out):
+    if VERBOSE:
+        print(i, chanmap[i], chanmap_readback[i])
+    if chanmap[i] != chanmap_readback[i]:
+        passed = False
+
+if passed:
+    print('#################')
+    print('# Test passed   #')
+    print('#################')
+else:
+    print('#################')
+    print('# Test Failed   #')
+    print('#################')
