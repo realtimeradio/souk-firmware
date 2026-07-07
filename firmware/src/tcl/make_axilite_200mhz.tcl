5bf741b33dc4c5c3faa1f9ef192518cd7135f9bc
JackH <jack@realtimeradio.co.uk>
2026-07-07 16:09:48 +0000

    Rebuild with 200MHz AXI clock

---
+++ firmware/src/tcl/make_axilite_200mhz.tcl
@@ -0,0 +1,13 @@
+# Hackery to get 200 MHz AXI clock
+set AXI_CLK_MHZ 200
+# Set MPSoC PL clock output to 200 MHz
+set_property -dict [list CONFIG.PSU__CRL_APB__PL0_REF_CTRL__FREQMHZ $AXI_CLK_MHZ] [get_bd_cells mpsoc]
+# Readback actual clock rate after PLL rounding
+set axi_clk_hz [get_property CONFIG.FREQ_HZ [get_bd_pins mpsoc/pl_clk0]]
+set axi_clk_mhz [expr {$axi_clk_hz / 1000000.0} ]
+# Set RFDC AXI-Lite interface clock
+set_property -dict [list CONFIG.Axiclk_Freq $axi_clk_mhz] [get_bd_cells usp_rf_data_converter_0]
+# Set block diagram output clock port rate
+set_property -dict [list CONFIG.FREQ_HZ $axi_clk_hz] [get_bd_ports pl_sys_clk]
+# Update clock rate of output MAXI interface by associating with clock port
+set_property CONFIG.ASSOCIATED_BUSIF {M_AXI} [get_bd_ports /pl_sys_clk]
