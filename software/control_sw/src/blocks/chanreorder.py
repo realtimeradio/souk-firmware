00ddc9ee13e657467a0c6d1b451b5499ced4c84f
JackH <jack@realtimeradio.co.uk>
2026-07-09 13:44:38 +0000

    Update discard bit location for v 7.12 firmware with 16-bit map

--- software/control_sw/src/blocks/chanreorder.py
+++ software/control_sw/src/blocks/chanreorder.py
@@ -603,9 +603,9 @@ class ChanReorderMultiSampleIn(ChanReorder):
             self.set_channel_outmap(chan_order)
 
 class VaccReorderMultiSampleIn(ChanReorderMultiSampleIn):
-    DISCARD_BIT = np.uint32(2**31) # 0x80000000
+    DISCARD_BIT = 0x8000
     DISCARD_BIN = 0 | DISCARD_BIT # doesnt matter what the address is when discard bit set
-    ADDR_MASK   = DISCARD_BIT-1 # 0x7FFFFFFF
+    ADDR_MASK   = DISCARD_BIT-1 # 0x7FFF
     _map_format = '>i2' # CASPER library-defined map word format
 
     def set_channel_inmap(self, inmap):
